682ebc964f9d0a642c08681f6f12878e44b4ea4e
chahinez <c.bounoua@esi-sba.dz>
2024-10-21 08:45:24 +0100

    Résolution des conflits

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'src', 
   plugins: [react()],
-  css:{
-    modules:{
-      localsConvention:"camelCase",
-    }
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+    },
   },
   build: {
-    outDir: 'dist',  // Indique le dossier de sortie
+    outDir: '../dist', 
     rollupOptions: {
       input: {
-        main: 'index.html',  // Assure-toi que Vite utilise ton index.html
+        main: 'index.html', 
       },
     },
   },
-})
+});
